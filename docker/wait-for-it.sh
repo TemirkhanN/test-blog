dc15aace3118e9ce2c09fd25629595ebf39a79ff
Temirkhan <TemirkhanN@users.noreply.github.com>
2017-06-29 17:39:45 +0300

    Update wait-for-it.sh

--- docker/wait-for-it.sh
+++ docker/wait-for-it.sh
@@ -2,9 +2,19 @@
 # wait-for-postgres.sh
 
 cd /var/www/html
+
 composer install --prefer-source --no-interaction
-chown -R www-data:www-data ./var
 rm -rf var/cache/*
+chown -R www-data:www-data var
+
+echo db:5432:blog_db:blog:some_pass > ~/.pgpass
+chmod 0600 ~/.pgpass
+
+until psql -h "db" -U "blog" -d "blog_db"; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
 
 php bin/console doctrine:schema:create
 
